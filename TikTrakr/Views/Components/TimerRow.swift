09fe442085a9c7464ea1f5b9aad1689dd71737eb
Alexis Creuzot <alexis.creuzot@gmail.com>
2023-04-06 11:32:33 -0600

    Improved TimerRow

--- TikTrakr/Views/Components/TimerRow.swift
+++ TikTrakr/Views/Components/TimerRow.swift
@@ -8,55 +8,81 @@
 import Foundation
 import SwiftUI
 
+import SwiftUI
+import SwiftUI
+
 struct TimerRow: View {
     let timer: TimerModel
-    
+
     @Binding var selectedTimer: TimerModel?
     @State private var elapsedTime = ""
 
-    let timerFormatter: RelativeDateTimeFormatter = {
-        let formatter = RelativeDateTimeFormatter()
-        formatter.unitsStyle = .full
-        return formatter
-    }()
-    
-    private func isPastDate() -> Bool {
-        return timer.startDate < Date()
-    }
+    private let timerFormatter = createTimerFormatter()
 
     var body: some View {
         HStack {
-            VStack(alignment: .leading, spacing: 8) {
-                Text(timer.title)
-                    .font(.headline)
-                Text(elapsedTime)
-                    .font(.title3)
-                    .fontWeight(.bold)
-                    .foregroundColor(isPastDate() ? .orange : .blue)
+            timerStateIcon
+                .foregroundColor(isPastDate ? .orange : .blue)
+                .padding(.trailing, 8)
 
+            VStack(alignment: .leading, spacing: 8) {
+                timerTitleText
+                elapsedTimeText
             }
+
             Spacer()
         }
-        .buttonStyle(BorderlessButtonStyle())
         .padding()
-        .background(Color(.systemGray6))
-        .cornerRadius(10)
-        .shadow(color: .gray.opacity(0.3), radius: 5, x: 0, y: 5)
-        .onTapGesture {
-            selectedTimer = timer
-        }
-        .onAppear {
+        .background( RoundedRectangle(cornerRadius: 10)
+                        .fill(Color(.systemGray6))
+                        .shadow(color: .gray.opacity(0.3), radius: 5, x: 0, y: 5)
+                    )
+        .onTapGesture { selectedTimer = timer }
+        .onAppear(perform: setupElapsedTimeUpdater)
+    }
+}
+
+private extension TimerRow {
+    static func createTimerFormatter() -> RelativeDateTimeFormatter {
+        let formatter = RelativeDateTimeFormatter()
+        formatter.unitsStyle = .full
+        return formatter
+    }
+
+    var timerStateIcon: some View {
+        Image(systemName: isPastDate ? "timer" : "hourglass")
+            .font(.system(size: isPastDate ? 24 : 30, weight: .regular))
+    }
+
+    var timerTitleText: some View {
+        Text(timer.title)
+            .font(.system(size: 18, weight: .semibold))
+    }
+
+    var elapsedTimeText: some View {
+        Text(elapsedTime)
+            .font(.title3)
+            .fontWeight(.bold)
+            .foregroundColor(isPastDate ? .orange : .blue)
+    }
+
+    var isPastDate: Bool {
+        timer.startDate < Date()
+    }
+
+    func setupElapsedTimeUpdater() {
+        updateTimeElapsed()
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
             updateTimeElapsed()
-            Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
-                updateTimeElapsed()
-            }
         }
     }
 
-    private func updateTimeElapsed() {
+    func updateTimeElapsed() {
         elapsedTime = timerFormatter.localizedString(for: timer.startDate, relativeTo: Date())
     }
 }
+
+
 
 struct TimerRow_Previews: PreviewProvider {
     static var previews: some View {
